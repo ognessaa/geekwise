c0c7e21dbc35265e6df9763d625d4b41a2a7648c
student-05 <student-05>
2016-10-18 15:22:05 -0700

    oct18

---
+++ october12/js/script.js
@@ -0,0 +1,20 @@
+var span = document.querySelectorAll('legend span');
+var input = document.querySelectorAll('legend input');
+//console.log(span);
+console.log(input);
+for(var i = 0; i < input.length; i++){
+  input[i].addEventListener('click', function(){
+    //console.log(this);
+    //this.classList.toggle('placeholder');
+    console.log(this === document.activeElement);
+
+    if(this === document.activeElement){
+        this.previousElementSibling.classList.toggle('placeholder');
+      }else{
+        for(var j = 0; j < span.length; j++){
+          span[j].class.List.remove('placeholder');
+        }
+    }
+  });
+
+}
