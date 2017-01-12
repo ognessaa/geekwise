4401f12408b35088ac02725578081755ea760532
student-05 <student-05>
2017-01-12 15:22:37 -0800

    jan12-intensify

---
+++ jan12-intensify/js/script.js
@@ -0,0 +1,15 @@
+var snOpen = document.getElementById("sn-open"),
+    sn = document.getElementById("sn"),
+    snClose = document.getElementById("sn-close");
+
+snOpen.addEventListener("click" , snAnimOpen);
+snClose.addEventListener("click" , snAnimClose);
+
+
+function snAnimOpen(){
+  sn.classList.add('open');
+}
+
+function snAnimClose(){
+  sn.classList.remove("open");
+}
