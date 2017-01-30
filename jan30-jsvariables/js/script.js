ba46574b4855a96ead93e573ed77364c1b1e3da0
geekwise-alonzo-yrigollen <geekwise.alonzo.yrigollen@gmail.com>
2017-01-30 15:50:21 -0800

    Jan3-JS

---
+++ jan30-jsvariables/js/script.js
@@ -0,0 +1,28 @@
+//alert('this an alert');
+
+
+/*
+  comment
+  out
+  multiple
+  lines
+*/
+var myNumberVar = 7; //Number object
+var myFloatVar = 5.2; //Float Object
+var myStringVar = "Vanessa Diaz"; //String Object
+var myBooleanVar = true; //Boolean Object
+var myArrayVar = ['one', 'stingray', 'banana', 8, false]; // Array Object
+var myObjectVar = {
+  make: 'Nissan',
+  model: 'Altima',
+  color: "White",
+  year: 2005 ,
+//Object Object
+};
+
+console.log ( typeof myNumberVar);
+console.log (typeof myFloatVar);
+console.log (typeof myStringVar);
+console.log (typeof myBooleanVar);
+console.log (typeof myArrayVar);
+console.log (myObjectVar.model);
