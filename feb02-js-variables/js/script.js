a41fddf3dbca2fa3f1298d877f67fb8e092d6d5b
geekwise-alonzo-yrigollen <geekwise.alonzo.yrigollen@gmail.com>
2017-02-02 15:45:20 -0800

    feb02-js-variables

---
+++ feb02-js-variables/js/script.js
@@ -0,0 +1,85 @@
+//var externalJavascript = "wha da actual heck"
+
+var camelCase;
+var not_camel_case;
+var camel_Case;
+var $dollarCase;
+var dollar$case;
+
+//var 1st Variable;
+var firstVar
+
+//var var;
+var variable
+
+//var UppercaseCamel
+//var SUPERUPERCASE
+
+/*
+JAVASCRIPT OBJECTS
+      number
+      string
+      boolean
+      float
+      array
+      object
+*/
+var myNumberObject = 3;
+
+var myNewNmyNumberObject = myNumberObject.toString();
+/*
+console.log(myNumberObject);
+console.log(myNewNmyNumberObject);
+*/
+        //string object
+/*
+var myStringObject = 'this is a string object';
+var myOtherstring object = 'this is another string object';
+console.log(myStringObject, myOtherstring);
+*/
+
+        //boolean object
+/*
+var myBooleanTrue = false;
+var myBooleanFalse = true;
+*/
+
+        //float object
+/*
+var myFloatObject
+*/
+
+        //array object
+var myArrayObject = ['red' , 8 , true , ['r' , 'b' , 'g']];
+//console.log(myArrayObject);
+
+      // object object
+var myObjectObject = {
+  make: 'Nissan',
+  model: 'Altima',
+  color: "White",
+  year: 2005 ,
+  package: function(){
+      alert(this.make);
+  }
+};
+
+
+// JAVASCRIPT MATH
+
+var a = 5;
+var b = 10;
+
+var c = a + b;
+
+
+var micaela = 10;
+var waldo = 10;
+
+
+var alf = micaela * waldo;
+var rosa = alf / waldo;
+var rigo = micaela - waldo;
+
+
+console.log(alf, rosa, rigo);
